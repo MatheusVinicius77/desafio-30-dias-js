6e468f99c9591eb268a54796677e45f7cf5088c2
MatheusVinicius77 <matheusviniciusbus7@hotmail.com>
2024-01-31 12:46:33 -0300

    fix: refatora desafio 3

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,20 +1,11 @@
-const spacingRange = document.querySelector('#spacing');
-const blurRange = document.querySelector('#blur');
-const baseRange = document.querySelector('#base');
-const image = document.getElementsByTagName('img').item(0);
-
-image.style.background = baseRange.value;
-image.style.padding = spacingRange.min + 'px';
-image.style.filter = `blur(${blurRange.value}px)`;
-
-spacingRange.addEventListener('input', (value) => {
-    image.style.padding = `${value.target.value}px`;
-});
-
-base.addEventListener('input', (value) => {
-    image.style.background = value.target.value;
-});
-
-blurRange.addEventListener('input', (value) => {
-    image.style.filter = `blur(${value.target.value}px)`;
-});
+const inputs = document.querySelectorAll('input')
+inputs.forEach(input=>{
+    input.addEventListener('input',(e)=>{
+        const root = document.documentElement
+        const element = e.target
+        root.style.setProperty(
+            `--${element.name}`, 
+            `${(element.value)}${(element.name !== 'base')?'px':''}`
+            )
+    })
+})
